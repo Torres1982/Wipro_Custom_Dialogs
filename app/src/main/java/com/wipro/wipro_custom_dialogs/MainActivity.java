264dc0ea785c18e461c678237169f2a33123d6a5
Artur Torres <artur.sukiennik82@gmail.com>
2019-06-14 15:52:33 +0100

    Date and Time pickers applied, working on button clicks.

--- app/src/main/java/com/wipro/wipro_custom_dialogs/MainActivity.java
+++ app/src/main/java/com/wipro/wipro_custom_dialogs/MainActivity.java
@@ -1,34 +1,44 @@
 package com.wipro.wipro_custom_dialogs;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    Button alertButton, dateButton, timeButton;
-    TextView textViewDate, textViewTime;
+    private TextView textViewDate, textViewTime;
+    private int year, month, day;
+    private int hour, minute;
+    private final Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        alertButton = findViewById(R.id.button_alert);
-        dateButton = findViewById(R.id.button_date);
-        timeButton = findViewById(R.id.button_time);
+        Button alertButton = findViewById(R.id.button_alert);
+        Button dateButton = findViewById(R.id.button_date);
+        Button timeButton = findViewById(R.id.button_time);
         textViewDate = findViewById(R.id.text_view_date);
         textViewTime = findViewById(R.id.text_view_time);
 
-        setButtonListener(alertButton, this);
+        setAlertButtonListener(alertButton, this);
+        setDateButtonListener(dateButton, this);
+        setTimeButtonListener(timeButton, this);
     }
 
-    // Set the Listener for buttons
-    private void setButtonListener(Button button, final Context context) {
+    // Set the Listener for Alert Button
+    private void setAlertButtonListener(Button button, final Context context) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +50,55 @@ public class MainActivity extends AppCompatActivity {
                         dialog.cancel();
                     }
                 }).show();
+            }
+        });
+    }
+
+    // Set the Listener for the Date Button
+    private void setDateButtonListener(Button button, final Context context) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                        year = selectedYear;
+                        month = selectedMonth;
+                        day = selectedDay;
+
+                        textViewDate.setText(new StringBuilder()
+                                .append(month + 1).append("-")
+                                .append(day).append("-")
+                                .append(year).append(" "));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    // Set the Listener for the Time Button
+    private void setTimeButtonListener(Button button, final Context context) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hour = calendar.get(Calendar.HOUR_OF_DAY);
+                minute = calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                        hour = selectedHour;
+                        minute = selectedMinute;
+
+                        textViewTime.setText(new StringBuilder().append(hour).append(":").append(minute));
+                    }
+                }, hour, minute, true);
+                timePickerDialog.show();
             }
         });
     }
